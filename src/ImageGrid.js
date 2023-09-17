9c2d9665c3883866b30c0eeb0a511f291ee27b5f
RDxR10 <risirajdey@gmail.com>
2023-09-18 01:21:44 +0530

    Update ImageGrid.js

--- src/ImageGrid.js
+++ src/ImageGrid.js
@@ -1,17 +1,26 @@
-// src/ImageGrid.js
 import React from 'react';
-import images from './images';
+import images from './images'; 
 
-function ImageGrid({ onImageClick }) {
+function ImageGrid({ onImageClick, upvotedImages, toggleUpvote }) {
   return (
     <div className="image-grid">
       {images.map((image, index) => (
         <div
           key={index}
-          className="image-item"
+          className={`image-item ${upvotedImages.includes(image) ? 'upvoted' : ''}`}
           style={{ backgroundImage: `url(${image})` }}
           onClick={() => onImageClick(image)}
-        />
+        >
+          <button
+            className={`upvote-button ${upvotedImages.includes(image) ? 'upvoted' : ''}`}
+            onClick={(e) => {
+              e.stopPropagation();
+              toggleUpvote(image);
+            }}
+          >
+            Upvote
+          </button>
+        </div>
       ))}
     </div>
   );
